49b35540d5a6292832135ed77e5e2895e80010f3
Koloina <rabenjamu@gmail.com>
2023-07-07 16:25:44 +0400

    controle mail

--- routes/login.js
+++ routes/login.js
@@ -7,26 +7,41 @@ router.post('/', (req, res) => {
     //console.log(data);
     //console.log(data.mail);
     mail=data.mail;
-    mdp=data.mdp;
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'site_rencontre'
-    })
+    var res1=String(mail)
+    .toLowerCase()
+    .match(/^[a-zA-Z0-9.]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+$/);
+    //console.log(res1);
+    var erreur=[];
+    if(res1!=null){
+        //console.log(erreur.erreur)
+        mdp=data.mdp;
+        const connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'site_rencontre'
+        })
 
-    connection.connect()
-    requete="select * from user where mail='"+mail+"' and mdp=sha1('"+mdp+"')";
-    //connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-    connection.query(requete, (err, rows, fields) => {
-        if (err) throw err
+        connection.connect()
+        requete="select * from user where mail='"+mail+"' and mdp=sha1('"+mdp+"')";
+        //connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+        connection.query(requete, (err, rows, fields) => {
+            if (err) throw err
 
-        //console.log( rows)
-        reponse=rows;
-        res.send(reponse);
-    })
-
-    connection.end();
+            //console.log( rows)
+            reponse=rows;
+            if(reponse.length==0){
+                res.send({erreur:"Erreur d'authentification"})
+            }else{
+                res.send(reponse);
+            }
+        })
+        connection.end();
+    }else{
+        erreur={erreur:'Mail invalide'};
+        //res.send('ok erreur');
+        res.send(erreur);
+    }
     
 })
 
