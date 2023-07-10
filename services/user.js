42bb19e9ca423afdd6a5190cc9c84d41e56913bc
Tahiana <tahianarazakanome@gmail.com>
2023-07-10 16:51:42 +0300

    fix registration

--- services/user.js
+++ services/user.js
@@ -4,8 +4,8 @@ const register = async (req, res) => {
     let data = req.body;
     // console.log(data.nom);
 
-    const [result] = await pool.query(`INSERT INTO user(nom, prenom, mail, mdp, pseudo, sexe, dateDeNaissance, ville, nation, orientationSxl) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [data.nom, data.prenom, data.mail, data.mdp, data.pseudo, data.sexe, data.dateDeNaissance, data.ville, data.nation, data.orientationSxl])
-    return result.insertId
+    const [result] = await pool.query(`INSERT INTO user(nom, prenom, mail, mdp, pseudo, sexe, dateDeNaissance, ville, nation, orientationSxl) VALUES (?, ?, ?, sha1(?), ?, ?, ?, ?, ?, ?)`, [data.nom, data.prenom, data.mail, data.mdp, data.pseudo, data.sexe, data.dateDeNaissance, data.ville, data.nation, data.orientationSxl])
+    res.status(201).json(result.insertId);
 }
 
 const getUser = async (req, res) => {
