bd723616bbeae106f31815ddfd5f1d4c54a32290
Koloina <rabenjamu@gmail.com>
2023-07-17 14:56:11 +0400

    recherche multi-criteres
    -requete age
    -requete genre

--- routes/users-list.js
+++ routes/users-list.js
@@ -15,7 +15,26 @@ router.post('/', (req, res) => {
     let data=req.body
     //console.log(data)
     var id_user=data.id
+    let filtres=data.filtres
     var requete='SELECT * from user where id!='+id_user
+    if(filtres.length>0){
+      if(filtres[0]!=''){
+        requete+=' and sexe="'+filtres[0]+'"'
+      }
+      if(filtres[1]!=''){
+        if(filtres[1]=='60'){
+          requete+=' and TIMESTAMPDIFF( year, dateDeNaissance, now( ) ) >='+60;
+        }
+        else{
+          requete+=' and TIMESTAMPDIFF( year, dateDeNaissance, now( ) ) >='+filtres[1]
+          requete+=' and TIMESTAMPDIFF( year, dateDeNaissance, now( ) )<'+filtres[2]
+        }
+      }
+    }    
+    //console.log(requete)
+    /*if(filtres[1]!=''){
+      if(filtres[1])
+    }*/
     connection.query(requete, (err, rows, fields) => {
         if (err) throw err
       
