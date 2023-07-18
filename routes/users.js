458d96b58d611d972c8723ec390fedecfe0ace97
Tahiana <tahianarazakanome@gmail.com>
2023-07-18 11:31:26 +0300

    fix registration

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { register, getUser } = require('../services/user');
+const { register, getUser, getUserByPseudo, getUserByEmail } = require('../services/user');
 
 router.route('/')
 .post(register)
 .get(getUser)
+
+router.get('/pseudo/:pseudo', getUserByPseudo)
+router.get('/email/:email', getUserByEmail)
 
 
 module.exports = router;
--- services/user.js
+++ services/user.js
@@ -4,7 +4,7 @@ const register = async (req, res) => {
     let data = req.body;
     // console.log(data.nom);
 
-    const [result] = await pool.query(`INSERT INTO user(nom, prenom, mail, mdp, pseudo, sexe, dateDeNaissance, ville, nation, orientationSxl) VALUES (?, ?, ?, sha1(?), ?, ?, ?, ?, ?, ?)`, [data.nom, data.prenom, data.mail, data.mdp, data.pseudo, data.sexe, data.dateDeNaissance, data.ville, data.nation, data.orientationSxl])
+    const [result] = await pool.query(`INSERT INTO user(nom, prenom, mail, mdp, pseudo, sexe, dateDeNaissance, ville, nation, orientationSxl, photoDeProfile) VALUES (?, ?, ?, sha1(?), ?, ?, ?, ?, ?, ?, ?)`, [data.name, data.firstname, data.email, data.password, data.pseudo, data.gender, data.date_of_birth, data.city, data.nationality, data.sexual_orientation, data.profile_picture])
     res.status(201).json(result.insertId);
 }
 
@@ -13,7 +13,19 @@ const getUser = async (req, res) => {
     res.json(user);
 }
 
+const getUserByPseudo = async (req, res) => {
+    const [user] = await pool.query("SELECT * FROM user WHERE pseudo=?", [req.params.pseudo]);
+    res.json(user);
+}
+
+const getUserByEmail = async (req, res) => {
+    const [user] = await pool.query("SELECT * FROM user WHERE mail=?", [req.params.email]);
+    res.json(user);
+}
+
 module.exports = { 
     register,
     getUser,
+    getUserByPseudo,
+    getUserByEmail
 };
