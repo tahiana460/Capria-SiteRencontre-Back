905b151f2abb47114c23488397930685dc032822
Tahiana <tahianarazakanome@gmail.com>
2023-07-20 15:13:32 +0300

    some fixes

--- services/user.js
+++ services/user.js
@@ -2,7 +2,6 @@ const { pool } = require('../database');
 
 const register = async (req, res) => {
     let data = req.body;
-    // console.log(data.nom);
 
     const [result] = await pool.query(`INSERT INTO user(nom, prenom, mail, mdp, pseudo, sexe, dateDeNaissance, ville, nation, orientationSxl, photoDeProfile) VALUES (?, ?, ?, sha1(?), ?, ?, ?, ?, ?, ?, ?)`, [data.name, data.firstname, data.email, data.password, data.pseudo, data.gender, data.date_of_birth, data.city, data.nationality, data.sexual_orientation, data.profile_picture])
     res.status(201).json(result.insertId);
