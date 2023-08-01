24e3bf9e3e3af8c16524ee5b97de4cded83fe9f9
Koloina <rabenjamu@gmail.com>
2023-08-01 14:03:16 +0400

    voir personnes vues profil

--- routes/vues.js
+++ routes/vues.js
@@ -11,14 +11,29 @@ const connection = mysql.createConnection({
 })
 
 connection.connect()
+router.get('/nb/:id', (req, res) => {
+    let data=req.params
+    //console.log(data)
+    var id_user=data.id
+    var requete='SELECT count(*) as nb from vues where visited_id='+id_user 
+    connection.query(requete, (err, rows, fields) => {
+        if (err) throw err
+        console.log( rows)
+        /* GET users listing. */
+        res.send(rows)
+        //reponse=rows[0].solution
+        
+    })
+});
+
 router.get('/:id', (req, res) => {
     let data=req.params
     //console.log(data)
     var id_user=data.id
-    var requete='SELECT count(*) as nb from vues where id='+id_user 
+    var requete='select user.* from user join vues on user.id=vues.visitor_id where vues.visited_id='+id_user 
     connection.query(requete, (err, rows, fields) => {
         if (err) throw err
-        console.log(rows)
+        //console.log(rows)
         //console.log( rows)
         /* GET users listing. */
         res.send(rows)
