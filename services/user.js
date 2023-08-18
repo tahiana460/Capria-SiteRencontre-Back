f0a5e3a329e1300514e530891e3646258c683d27
Koloina <rabenjamu@gmail.com>
2023-08-18 12:03:11 +0400

    inscription, getUserByEmail
    - check mail existe

--- services/user.js
+++ services/user.js
@@ -3,8 +3,11 @@ const { pool } = require('../database');
 const register = async (req, res) => {
     let data = req.body;
 
-    const [result] = await pool.query(`INSERT INTO user(nom, prenom, mail, mdp, pseudo, sexe, dateDeNaissance, ville, nation, orientationSxl, profile_picture) VALUES (?, ?, ?, sha1(?), ?, ?, ?, ?, ?, ?, ?)`, [data.name, data.firstname, data.email, data.password, data.pseudo, data.gender, data.date_of_birth, data.city, data.nationality, data.sexual_orientation, data.profile_picture])
-    res.status(201).json(result.insertId);
+    const [result] = await pool.query(`INSERT INTO user(nom, prenom, mail, mdp, pseudo, sexe, dateDeNaissance, ville, nation, orientationSxl, photoDeProfil) VALUES (?, ?, ?, sha1(?), ?, ?, ?, ?, ?, ?, ?)`, [data.name, data.firstname, data.email, data.password, data.pseudo, data.gender, data.date_of_birth, data.city, data.nationality, data.sexual_orientation, data.profile_picture])
+    //console.log(result)
+    const ress={insertId:result.insertId,status:201,ok:true};
+    //res.status(201).json(result.insertId);
+    res.json(ress)
 }
 
 const getUser = async (req, res) => {
@@ -23,7 +26,22 @@ const getUserByPseudo = async (req, res) => {
 }
 
 const getUserByEmail = async (req, res) => {
-    const [user] = await pool.query("SELECT * FROM user WHERE mail=?", [req.params.email]);
+    var [user] = await pool.query("SELECT * FROM user WHERE mail=?", [req.params.email]);    
+    if(user.length==0){
+        const options = {method: 'GET'};
+        const api_key='70b908491d62418884377fefd4deca8c';
+        const mail=req.params.email;
+        const url='https://emailvalidation.abstractapi.com/v1/?api_key='+api_key+'&email='+mail
+        const response=await fetch(url, options)
+        /*.then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));*/
+        const resp=await response.json()
+        //console.log(resp)
+        if(resp.deliverability!='DELIVERABLE'){
+            user={erreur:'e-mail non existance'}
+        }
+    }
     res.json(user);
 }
 
