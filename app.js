b700cf856057dd37bce303e8b9ab7354f8cc9a0e
Tahiana <tahianarazakanome@gmail.com>
2023-09-29 15:30:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -35,11 +35,11 @@ app.use(express.urlencoded({ extended: false }));
 
 // const suppliersRouter = require("./routes/suppliers")
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(cors({
   origin: "*"
